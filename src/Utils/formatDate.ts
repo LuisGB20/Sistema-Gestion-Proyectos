1afc96bf6f541b4c678036222025d9ae74e402d8
LuisGB20 <luisalbertogomezbroca@gmail.com>
2025-03-11 18:45:39 -0500

    fix: formateo fecha

--- src/Utils/formatDate.ts
+++ src/Utils/formatDate.ts
@@ -1,4 +1,3 @@
 export const formatDate = (date: string) => {
     return new Date(date).toLocaleDateString('es-MX', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' });
 };
-git 
